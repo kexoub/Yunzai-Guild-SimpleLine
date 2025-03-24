7f669b4adf169acf4d99f00e6f0bb972cde02dd4
Murasame <134369576+kexoub@users.noreply.github.com>
2025-03-25 05:10:11 +0800

    Update Yunzai-TRSS-ubuntu-test.sh

--- Yunzai-TRSS-ubuntu-test.sh
+++ Yunzai-TRSS-ubuntu-test.sh
@@ -14,6 +14,9 @@ apt update -y && apt upgrade -y
 apt install curl wget unzip git
 #下载常用软件
 
+apt install libnss3 libatk1.0-0 libx11-xcb1-  -y
+
+apt install ca-certificates fonts-liberation libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils libxkbcommon0 -y
 
 apt install python3-pip
 #建议不要动
