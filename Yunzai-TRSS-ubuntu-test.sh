dc46c8dac9eeeb8c8ca2c7e34c74f3266b388732
Murasame <134369576+kexoub@users.noreply.github.com>
2025-03-25 14:05:45 +0800

    Update Yunzai-TRSS-ubuntu-test.sh

--- Yunzai-TRSS-ubuntu-test.sh
+++ Yunzai-TRSS-ubuntu-test.sh
@@ -16,9 +16,6 @@ apt install libnss3 libatk1.0-0 libx11-xcb1-  -y
 
 
 
-sudo apt-get install -y libxss1 libgbm-dev libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libxkbcommon0 libxcomposite1 libxdamage1 libxrandr2 libgtk-3-0
-
-apt install ca-certificates fonts-liberation libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils libxkbcommon0 -y
 
 apt install python3-pip
 #建议不要动
